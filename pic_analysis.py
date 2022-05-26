eecd7fa2b92b244533b2b080ee223a20d050259e
m-schmutz <marschm131@gmail.com>
2022-05-25 19:07:05 -0700

    commas

--- pic_analysis.py
+++ pic_analysis.py
@@ -13,12 +13,12 @@ wk(0)
 
 img_grey = cv2.cvtColor(img_white, cv2.COLOR_BGR2GRAY)
 cv2.imshow('img_grey', img_grey)
-cv2,waitKey(0)
+cv2.waitKey(0)
 
 retval, thresholded = cv2.threshold(img_grey, 200, 255, cv2.THRESH_BINARY_INV)
 
 cv2.imshow('threshold', thresholded)
-cv2,waitKey(0)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
