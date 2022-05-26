7cc0029d19763a0fb8b1615301229c399b13b91c
m-schmutz <marschm131@gmail.com>
2022-05-26 11:57:10 -0700

    beginning image to text

--- pic_analysis.py
+++ pic_analysis.py
@@ -1,24 +1,26 @@
 #!./env/bin/python3
-
-from re import sub
 import cv2
-from cv2 import boundingRect
+import pytesseract
+import numpy as np
+
 
 blue = (0, 0, 255)
-# img2_grey = cv2.cvtColor(img_black, cv2.COLOR_BGR2GRAY)
-# cv2.imshow('img2_grey', img2_grey)
-# cv2.waitKey(0)
+ROW_LENGTH = 5
 
-# mask = cv2.inRange(img2_grey, 15, 25)
+def invert_ltr(si):
+    lower_bound = np.array([200, 200, 200])
+    upper_bound = np.array([256, 256, 256])
 
-# test = cv2.bitwise_or(mask, img2_grey)
+    mask = cv2.inRange(si, lower_bound, upper_bound)
+    cv2.imwrite('./mask.png', mask)
+    
+    
 
-# cv2.imshow('test', test)
-# cv2.waitKey(0)
 
 def get_subimages(img):
     img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     mask = cv2.inRange(img_grey, 15, 25)
+
     masked = cv2.bitwise_or(mask, img_grey)
     retval, thresh = cv2.threshold(masked, 200, 255, cv2.THRESH_BINARY_INV)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -28,16 +30,47 @@ def get_subimages(img):
         subimages = [img[y:y+h, x:x+w]] + subimages
     return subimages
 
+def get_words(subimages):
+    rows = len(subimages) // 5
+    guesses = [''] * rows
+
+    si = subimages[0]
+
+    cv2.imwrite('./si.png', si)
+
+    si_rgb = cv2.cvtColor(subimages[0], cv2.COLOR_BGR2RGB)
+
+    cv2.imwrite('./si_rgb.png', si_rgb)
+
+    invert_ltr(si_rgb)
+
+    # print(pytesseract.image_to_string(si_rgb))
+
+
+
+    # for i in range(rows):
+    #     j = i * ROW_LENGTH
+    #     guesses[i] = ''.join([si for si in subimages[j:j + ROW_LENGTH]])
+    # return guesses
+        
+        
+        
+
+
 img_white = cv2.imread('./test_images/wordle.jpg')
-cv2.imwrite('./white.png', img_white)
 
-img_black = cv2.imread('./test_images/wordle.png')
-
-
-subimages = get_subimages(img_black)
-
-for i, si in enumerate(subimages):
-    cv2.imwrite(f'./cropped/si{i}.png', si)
+# img_black = cv2.imread('./test_images/wordle.png')
 
 
+subimages = get_subimages(img_white)
+
+# for i, si in enumerate(subimages):
+#     cv2.imwrite(f'./cropped/si{i}.png', si)
+
+#subimages = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+get_words(subimages)
+
+#print(guesses)
 
