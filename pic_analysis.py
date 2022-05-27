61ef1650ae74664ca80f905586f98b02c57656f3
m-schmutz <marschm131@gmail.com>
2022-05-26 17:53:10 -0700

    yay progress

--- pic_analysis.py
+++ pic_analysis.py
@@ -1,23 +1,37 @@
 #!./env/bin/python3
-from tkinter.tix import ROW
-from turtle import end_fill
+from string import ascii_lowercase, ascii_uppercase
 import cv2
 import pytesseract
 import numpy as np
 from PIL import Image
+import re
 
 ROW_LENGTH = 5
 
+YELLOW_RANGE = ()
+
+GREY_RANGE = ()
+
+WHITE_RANGE = (np.array([200, 200, 200]), np.array([256, 256, 256]))
+
+
+subs = {'l': 'I', '0': 'O'}
+
+
+TWINS = r'^(A-Z)\1$'
+res = re.search
 #checks output of pytesseract
 def check_ltr(ltr):
-    #only consider the first letter given and strip() new lines
-    ltr = ltr[0].strip()
-    #tesseract thinks that 'I' is 'l', so reverse this
-    # this is safe to do because Wordle only has letters as uppercase
-    if ltr == 'l':
-        ltr = 'I'
-    if ltr == '0':
-        ltr = 'O'
+    l_ltr = list(ltr)
+    print(f'ltr: {ltr}')
+    try:
+        ltr = subs[ltr]
+    except:
+        pass
+    
+    if res(TWINS, ltr.upper()):
+        ltr = ltr[0]
+
     #return uppercase letter
     return ltr.upper()
 
@@ -25,18 +39,17 @@ def check_ltr(ltr):
 def get_ltr(si):
     #create upper and lower bound arrays
     #that represent range of white-ish pixels
-    lower_bound = np.array([200, 200, 200])
-    upper_bound = np.array([256, 256, 256])
+    lower_bound, upper_bound = WHITE_RANGE
     #mask is the image that grabs only pixels that are white
     mask = cv2.inRange(si, lower_bound, upper_bound)
+   
     #inverse image so that letter is black on white background
     mask = cv2.bitwise_not(mask)
     #convert to RGB so that pytesseract can read it
     mask_rgb = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
     #get output from pytesseract
-    ltr = pytesseract.image_to_string(mask_rgb, lang='eng', config = f'--psm 10 --oem 1 -c tessedit_char_blacklist=$')
-    
-    #might be useful in the future. Stackoverflow: https://stackoverflow.com/questions/20831612/getting-the-bounding-box-of-the-recognized-words-using-python-tesseract#54059166
+    ltr = pytesseract.image_to_string(mask_rgb, lang='eng', config = f'--psm 10 --oem 1 -c tessedit_char_blacklist=$').strip()
+    # might be useful in the future. Stackoverflow: https://stackoverflow.com/questions/20831612/getting-the-bounding-box-of-the-recognized-words-using-python-tesseract#54059166
     # data = pytesseract.image_to_boxes(mask_rgb, lang='eng', config = f'--psm 10 --oem 1 -c tessedit_char_blacklist=$')
     # print(data)
 
@@ -45,12 +58,17 @@ def get_ltr(si):
     
 #takes in 
 def get_subimages(img):
+    # cv2.imshow('passed img', img)
     img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     mask = cv2.inRange(img_grey, 15, 25)
+    # cv2.imshow('mask', mask)
+    # cv2.imshow('img_grey', img_grey)
 
     masked = cv2.bitwise_or(mask, img_grey)
+    # cv2.imshow('masked', masked)
     retval, thresh = cv2.threshold(masked, 200, 255, cv2.THRESH_BINARY_INV)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.imshow('thresh', thresh)
     subimages = []
     for contour in contours:
         x,y,w,h = cv2.boundingRect(contour)
@@ -72,11 +90,31 @@ def get_guesses(subimages):
     return guesses
 
 
-img_white = cv2.imread('./test_images/wordle6.png')
+img_white = cv2.imread('./test_images/wordle5.png')
+
 
 subimages = get_subimages(img_white)
+
+si = subimages[0]
+
+# cv2.imshow('subimage', si)
+# cv2.waitKey(0)
+##########
+# GREEN  #  
+# R: 83  #
+# G: 141 #
+# B: 78  #
+##########
+print(str(ascii_uppercase))
+
+# GREEN_RANGE = (np.array([200, 200, 200]), np.array([256, 256, 256]))
+
+# lower_bound, upper_bound = GREEN_RANGE
+
+# mask = cv2.inRange(si, )
 
 guesses = get_guesses(subimages)
 print(guesses)
 
-
+cv2.waitKey(0)
+cv2.destroyAllWindows()
