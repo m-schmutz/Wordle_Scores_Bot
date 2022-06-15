75504a9af3b42f3f8d498fc7fdddcbcc6be09f01
m-schmutz <marschm131@gmail.com>
2022-06-15 02:11:08 -0700

    updates so that webscraping works

---
+++ get_solution_word.py
@@ -0,0 +1,23 @@
+#!./env/bin/python3
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+SOLUTION_OFFSET = 105
+END_OF_WORD = 110
+
+
+
+firefox_options = Options()
+firefox_options.add_argument('--headless')
+ 
+ 
+wd = webdriver.Firefox(options=firefox_options)
+
+wd.get("https://www.nytimes.com/games/wordle/index.html")
+r = wd.execute_script("return window.localStorage.getItem('nyt-wordle-state')")
+print(r)
+index = r.find("solution")
+print(index)
+
+print(r[SOLUTION_OFFSET:END_OF_WORD])
+ 
