ff2e839c85932f2c2f6cb29de0338d10ea566d71
Ryan <stachurr@oregonstate.edu>
2022-06-20 03:39:15 -0700

    added a method to obtain the WOTD + general file/filespace clean up

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 #!./env/bin/python3
-import bot
 
-#region Doin' your mom, doi-doin' your mom
-# Here we mimic Wordle's "Word of the Day" (WOTD) function and
-# use the same input data. 
-########## W.I.P. ########## W.I.P. ########## W.I.P. ########## W.I.P. ##########
-
-# import word_lists.HAX
-# import time
-# import datetime
-
-# SEC_PER_DAY = 86400
-
-# # GMT: Saturday, June 19, 2021 12:00:00 AM
-# epoch_start = int(datetime.datetime(2021, 6, 19, 0, 0, 0, 0).timestamp())
-# epoch_now   = int(time.time())
-# index       = (epoch_now - epoch_start) // SEC_PER_DAY
-# print(f"start = {epoch_start}\nnow = {epoch_now}\nindex = {index}")
-
-# ko_dict = word_lists.HAX.load_ko()
-# vals = list(ko_dict.values())
-# print("wotd = ", vals[index])
-#endregion
-
-
+import lib.bot as bot
 print("Starting bot...")
 bot.run()
+
+# import lib.wordle_requests as wreq
+# wotd = wreq.wotd()
+# print('WOTD =', wotd)
+
+# import cv2
+# import lib.wordle_image_processing as wip
+# from cv2 import imread
+# img = cv2.imread('./../test_images/dark1.png')
+# wip.get_guesses(img)
