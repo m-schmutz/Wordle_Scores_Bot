15712c9609c647a438c19029ca03f15989255c16
Ryan <stachurr@oregonstate.edu>
2022-07-03 00:13:29 -0700

    formalized

--- lib/wordle_requests_new.py
+++ lib/wordle_requests_new.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
-import json
+from json import loads as JSONloads
 
 options = FirefoxOptions()
 options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
+driver = webdriver.Firefox(options=options, service_log_path='./lib/geckodriver.log')
+
 driver.get('https://www.nytimes.com/games/wordle/index.html')
-var = driver.execute_script('return window.localStorage.getItem("nyt-wordle-state")')
-wotd = json.loads(var)['solution']
-print(f'The WOTD is \'{wotd}\'')
+# !!! DO NOT RUN !!! this literally consumed all of my memory. is it a bug? or do i misunderstand?
+# wotd = driver.execute_script('var d = window.localStorage.getItem("nyt-wordle-state"); return JSON.parse(d["solution"])')
+# !!! DO NOT RUN !!!
+nyt_wordle_state = driver.execute_script('return window.localStorage.getItem("nyt-wordle-state")')
+wotd = JSONloads(nyt_wordle_state)['solution']
+
+print('Word of the Day:', wotd)
