85087ec71749d3b810c8f16a997017b3f31d2977
Ryan <stachurr@oregonstate.edu>
2022-07-13 03:53:34 -0700

    filespace clean up

--- main.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-VENV_PATH = './wordle-venv'
-
-proc = subprocess.Popen([f'source {VENV_PATH}/bin/activate; python3 ./wordle_lib/bot.py; deactivate'],
-    shell=True, executable='/bin/bash')
-proc.wait()
-print('main.py> Done!')
