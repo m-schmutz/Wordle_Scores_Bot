bb5e1804b3c370d6d10dd07fc5d3667fab2e64d8
m-schmutz <marschm131@gmail.com>
2022-07-15 21:17:47 -0700

    reworked adding and updating user in database

--- lib/database.py
+++ lib/database.py
@@ -74,19 +74,25 @@ class BotDatabase:
         self._database.commit()
 
     def get_stats(self, username:str):
+        '''
+        Method returns the stats on the specified user
+        '''
         __cur = self._database.cursor()
         __raw = __cur.execute(f'''SELECT * FROM User_Data WHERE username = '{username}';''').fetchall()
         print(__raw)
 
-    def _get_update_values(self, solved:int, last_solve:int, date:int):
-        # determine whether solves will be incremented
+    def _get_update_values(self, solved:int, __last_solve:int, date:int):
+        # increase solves if user solved the wordle; otherwise solves stays the same
         __solves_update = 'solves + 1' if solved else 'solves'
 
-        # determine if user solved the wordle the previous day
-        __prev_solve = True if (date - __last_solve) == 1 else False
+        # increment streak if user has solved consecutively; otherwise streak will not be incremented
+        __continue_streak = True if (date - __last_solve) == 1 else False
 
-        # increase streak if user solved today and day before
-        if solved and __prev_solve:
+        # last solve is set to current date if wordle solved; otherwise last solve stays the same
+        __last_solve_update = date if solved else __last_solve
+
+        # increment streak 
+        if solved and __continue_streak:
             __streak_update = 'curr_streak + 1'
         
         # set streak to 1 if this is the start of a new streak
@@ -97,18 +103,93 @@ class BotDatabase:
         else:
             __streak_update = '0'
 
-    def _update_user(self, username:str, solved:int, guesses:int, greens:int, yellows:int, date:int):
+        # return computed values
+        return __solves_update, __streak_update, __last_solve_update
+
+    def _update_user(self, username:str, solved:bool, guesses:int, greens:int, yellows:int, date:int):
 
         # initialize cursor
         __cur = self._database.cursor()
 
-        # get last solve date
+        # get the last solve date for this user
         __last_solve = __cur.execute(f'''SELECT last_solve FROM User_Data WHERE username = '{username}';''').fetchone()[0]
 
-    def _add_user(self, username:str, solved:int, guesses:int, greens:int, yellows:int, date:int):
-        pass
+        # get the update values for the database
+        __solves_update, __streak_update, __last_solve_update = self._get_update_values(solved, __last_solve, date)
+
+        # execute sql script to update data in database for this user
+        __cur.executescript(f'''
+            UPDATE User_Data SET attempts = attempts + 1 WHERE username = '{username}';
+            UPDATE User_Data SET solves = {__solves_update} WHERE username = '{username}';
+            UPDATE User_Data SET guesses = guesses + {guesses} WHERE username = '{username}';
+            UPDATE User_Data SET greens = greens = greens + {greens} WHERE username = '{username}';
+            UPDATE User_Data SET yellows = yellows + {yellows} WHERE username = '{username}';
+            UPDATE User_Data SET curr_streak = {__streak_update} WHERE username = '{username}';
+            UPDATE User_Data SET last_solve = {__last_solve_update} WHERE username = '{username}'; 
+
+            UPDATE Group_Data SET attempts = attempts + 1;
+            UPDATE Group_Data SET solves = {__solves_update};
+            UPDATE Group_Data SET guesses = guesses + {guesses};
+            UPDATE Group_Data SET greens = greens + {greens};
+            UPDATE Group_Data SET yellows = yellows + {yellows};
+        ''')
+
+        # close cursor
+        __cur.close()
+
+        # commit changes to the database
+        self._database.commit()
+
+    def _add_user(self, username:str, solved:bool, guesses:int, greens:int, yellows:int, date:int):
+        
+        __cur = self._database.cursor()
+
+        # add user to the _users dictionary
+        self._users[username] = date
+
+        # attempts set to 1
+        __attempts_insert = 1
+        
+        # solves and streak set to 1 if solved; otherwise 0
+        __solves_insert, __streak_insert = 1 if solved else 0
+
+        # last_solve will set to todays date if solved; otherwise set to 0
+        __date_insert = date if solved else 0
+
+        # execute sql script to add new user to the database
+        __cur.executescript(f'''
+            INSERT INTO User_Data (
+                username, 
+                attempts, 
+                solves, 
+                guesses, 
+                greens, 
+                yellows, 
+                curr_streak, 
+                max_streak, 
+                last_solve)
+                Values (
+                    '{username}', 
+                    {__attempts_insert}, 
+                    {__solves_insert}, 
+                    {guesses}, 
+                    {greens}, 
+                    {yellows}, 
+                    {__streak_insert}, 
+                    {__streak_insert}, 
+                    {__date_insert});
+        ''')
+
+        # close the cursor
+        __cur.close()
+
+        # commit the changes to the database
+        self._database.commit()
 
     def submit_data(self, username:str, solved:bool, guesses:int, greens:int, yellows:int):
+        '''Given the username and info on attempt, user stats are updated in the database. A user is added to the database if
+        they are a new user. Method will raise DoubleSubmit exception if method is called on the same user twice or more on one day'''
+
         # grab todays date in integer form (YYYYMMDD)
         date = date_to_int()
 
@@ -140,109 +221,7 @@ class BotDatabase:
 
             
 if __name__ == '__main__':
-    # remove(DB_PATH)
-    # db = BotDatabase(DB_PATH)
+    remove(DB_PATH)
+    db = BotDatabase(DB_PATH)
 
-    # db.submit_data('mario', 1, 1, 1, 1, 1)
-
-    date = date_to_int()
-
-    __last_solve = 20220713
-
-    r = True if (date - __last_solve) == 1 else False
-
-
-    print(r)
-
-
-
-
-
-
-
-
-
-# # create cursor
-#         __cur = self._database.cursor()
-#         # __date = int(datetime.now().date().isoformat().replace('-', ''))
-#         __date = date
-
-#         # if user is in the database
-#         if username in self._users:
-
-#             # determine whether the solve stat will be incremented or not
-#             __solve_update = 'solves + 1' if solved else 'solves'
-
-#             # get value of the last solve date
-#             __last_solve = __cur.execute(f'''SELECT last_solve FROM User_Data WHERE username = '{username}';''').fetchone()[0]
-
-#             # determine if user solved the wordle the day before
-#             __prev_solved = True if (__date - __last_solve) == 1 else False
-
-#             # determine if user's current streak will be incremented or set to 0
-#             __curr_streak = 'curr_streak + 1' if solved and __prev_solved else '0'
-
-#             # determine if the last solve date will be updated or stay the same
-#             __date_update = str(__date) if solved else 'last_solve'
-
-#             # execute sql script to update the database
-#             __cur.executescript(f'''
-#                 UPDATE User_Data SET attempts = attempts + 1 WHERE username = '{username}';
-#                 UPDATE User_Data SET solves = {__solve_update} WHERE username = '{username}';
-#                 UPDATE User_Data SET guesses = guesses + {guesses} WHERE username = '{username}';
-#                 UPDATE User_Data SET greens = greens + {greens} WHERE username = '{username}';
-#                 UPDATE User_Data SET yellows = yellows + {yellows} WHERE username = '{username}';
-#                 UPDATE User_Data SET last_solve = {__date_update} WHERE username = '{username}';
-#                 UPDATE User_Data SET curr_streak = {__curr_streak} WHERE username = '{username}';
-
-#                 UPDATE Group_Data SET attempts = attempts + 1;
-#                 UPDATE Group_Data SET solves = {__solve_update};
-#                 UPDATE Group_Data SET guesses = guesses + {guesses};
-#                 UPDATE Group_Data SET greens = greens + {greens};
-#                 UPDATE Group_Data SET yellows = yellows + {yellows};
-#             ''')
-
-#             # close cursor to conserve memory
-#             __cur.close()
-
-#             # commit changes to the database
-#             self._database.commit()
-
-#         else:
-#             self._users.add(username)
-
-#             __solves_insert = '1' if solved else '0'
-#             __date_insert = str(__date) if solved else '0' 
-
-#             __cur.executescript(f'''
-#                 INSERT INTO User_Data (
-#                     username, 
-#                     attempts, 
-#                     solves, 
-#                     guesses, 
-#                     greens, 
-#                     yellows, 
-#                     curr_streak, 
-#                     max_streak, 
-#                     last_solve)
-#                     Values (
-#                         '{username}', 
-#                         1, 
-#                         {__solves_insert}, 
-#                         {guesses}, 
-#                         {greens}, 
-#                         {yellows}, 
-#                         {__solves_insert}, 
-#                         {__solves_insert}, 
-#                         {__date_insert});
-
-#                 UPDATE Group_Data SET attempts = attempts + 1;
-#                 UPDATE Group_Data SET solves = {__solves_insert};
-#                 UPDATE Group_Data SET guesses = guesses + {guesses};
-#                 UPDATE Group_Data SET greens = greens + {greens};
-#                 UPDATE Group_Data SET yellows = yellows + {yellows};
-            
-#             ''')
-            
-#             __cur.close()
-#             self._database.commit()
+    db._update_user('mario', True, 1, 1, 1, 20220717)
