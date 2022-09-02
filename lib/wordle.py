885f68444c815df7dbeadfd93526c8ea5de74127
m-schmutz <marschm131@gmail.com>
2022-09-01 22:37:38 -0700

    moved word lookup into wordle.py

--- lib/wordle.py
+++ lib/wordle.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from pytesseract import image_to_string
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from enum import Enum, auto
 from collections import Counter
 from typing import Tuple
@@ -15,14 +15,17 @@ from time import perf_counter
 from dataclasses import dataclass
 from sqlite3 import connect
 from random import choice
-from os import path
+from os import path, mkdir
 
 from requests import get
-from credentials import api_headers
-import json
+from lib.credentials import api_headers
+from json import loads
 
-import ansi
+from typing import Tuple
+from re import search, findall
+from pickle import load, dump
 
+import lib.ansi as ansi
 
 
 def timer(func):
@@ -54,6 +57,7 @@ class BaseStats:
         self.win_rate = float(win_rate) * 100
         self.streak = int(streak)
         self.max_streak = int(max_streak)
+
 
 class FullStats(BaseStats):
     '''FullStats for a user
@@ -105,6 +109,7 @@ class FullStats(BaseStats):
         self.green_rate = float(green_rate) * 100
         self.yellow_rate = float(yellow_rate) * 100
         self.last_win = datetime.strptime(str(last_win), '%Y%m%d').date()
+
 
 class UpdateValues:
     def __init__(self, raw:Tuple, win:bool, guesses:int, greens:int, yellows:int, uniques:int, date:int) -> None:
@@ -183,6 +188,7 @@ class UpdateValues:
         # yellow rate update value 
         self._yellow_rate_update = self._yellows_update / self._uniques_update
 
+
 class DoubleSubmit(Exception):
     '''Exception raised if user attempts to submit twice on the same day'''
     
@@ -198,6 +204,7 @@ class DoubleSubmit(Exception):
     # print string including username of user that has attempted to submit twice
     def __str__(self):
         return f'{self.username} has already submitted today'
+
 
 class BotDatabase:
     '''Database class. Contains one member _database. 
@@ -476,10 +483,12 @@ class WordleGame:
     totalCorrect: int
     totalMisplaced: int
 
+
 class InvalidGame(Exception):
     def __init__(self, reason: str, *args: object) -> None:
         super().__init__(*args)
         self.reason = reason
+
 
 class CharScore(Enum):
     CORRECT = auto()
@@ -497,6 +506,7 @@ class CharScore(Enum):
 
         raise AssertionError(f'Unexpected CharScore "{self.name}"')
 
+
 class SubmissionReply(discord.Embed):
     def __init__(self, username: str, stats: BaseStats):
         super().__init__(
@@ -511,6 +521,7 @@ class SubmissionReply(discord.Embed):
         self.add_field(name='Win Rate', value=f'{stats.win_rate:.02f}%', inline=False)
         self.add_field(name='Streak', value=stats.streak, inline=False)
         self.add_field(name='Max Streak', value=stats.max_streak, inline=False)
+
 
 class WordleScraper:
     """Keeps track of the Word of the Day. Uses Selenium to scrape the NYTimes Wordle webpage."""
@@ -552,7 +563,6 @@ class WordleScraper:
                 return prev_wotd
 
         return self._wotd
-
 
 
 class WordleBot(commands.Bot):
@@ -781,7 +791,6 @@ class WordleBot(commands.Bot):
         print(f'{self.user} ready!')
 
 
-
 class WordInfo:
     '''
     Given a word, the object will store the word's definitions, examples and average frequency per 1 million words
@@ -798,13 +807,13 @@ class WordInfo:
         self.word = wotd
 
         # get json data for the definition(s)
-        def_json = json.loads(get(f'https://wordsapiv1.p.rapidapi.com/words/{wotd}/definitions', headers=api_headers).text)
+        def_json = loads(get(f'https://wordsapiv1.p.rapidapi.com/words/{wotd}/definitions', headers=api_headers).text)
         
         # get json data for the frequency 
-        freq_json = json.loads(get(f'https://wordsapiv1.p.rapidapi.com/words/{wotd}/frequency', headers=api_headers).text)
+        freq_json = loads(get(f'https://wordsapiv1.p.rapidapi.com/words/{wotd}/frequency', headers=api_headers).text)
 
         # get the json data for examples
-        exam_json = json.loads(get(f'https://wordsapiv1.p.rapidapi.com/words/{wotd}/examples', headers=api_headers).text)
+        exam_json = loads(get(f'https://wordsapiv1.p.rapidapi.com/words/{wotd}/examples', headers=api_headers).text)
         
         # store the definitions of word sorted by part of speech
         self.definitions = self.parse_definitions(def_json['definitions'])
@@ -860,3 +869,304 @@ class WordInfo:
 
     def __str__(self) -> str:
         return f'{self.definitions = }\n{self.per_million = }\n{self.examples = }'
+
+
+class WordLookup:
+    '''
+    WordLookup class. Provides an API for getting words and dates from the wordle website
+    Words are stored in pickle files
+    #### Methods
+    ---
+    - lookup_by_date(): Given a datetime object, the method returns the word that is mapped at that date
+    - lookup_by_word(): Given a word, the method will return the datetime object that is mapped at that word
+    - get_valid_words(): Method returns the list of valid words that wordle will accept as guesses
+    - get_word_index(): Given a word, the method will return the index of the word contained in word_order
+    '''
+
+    def __init__(self) -> None:
+        # check if files need to be generated
+        if not self._check_existing():
+            # generate files and registry
+            self._gen_files()
+
+ 
+    def lookup_by_date(self, dtime:datetime) -> str:
+        '''Takes in a datetime object and returns the word mapped at that date'''
+        # load in the year dictionary from pickle file
+        year_dict = self._load_object(dtime.year)
+
+        # return the word at the datetime passed
+        return year_dict[dtime]   
+
+    
+    def lookup_by_word(self, word:str) -> datetime:
+        '''Takes in a word and returns the datetime object mapped at that word'''
+        # load the word dictionary from pcilel file
+        word_dict = self._load_object('reverse')
+
+        # return datetime at word passed
+        return word_dict[word]
+
+  
+    def get_valid_words(self) -> list:
+        '''Returns the list of valid words retrieved from the website'''
+        # load the valid_words list from pickle file
+        valid_words = self._load_object('valid_words')
+
+        # return the list
+        return valid_words
+
+ 
+    def get_word_index(self, word:str) -> int:
+        '''Takes in a word and returns the index of the word in the word_order list'''
+        # load the word_order list form the pickle file
+        word_order = self._load_object('word_order')
+
+        # return the index of the word
+        return word_order.index(word)
+
+  
+    def _get_word_order(self) -> list[str]:
+        word_order = self._load_object('word_order')
+
+        return word_order
+
+    ####################################################
+    # methods for generating files
+
+    def _check_existing(self) -> bool:
+        # get the parent directory path
+        parent_dir = './lib/wordle_pickles/{prefix}.pkl'[:20]
+
+        # check if the parent directory exists
+        if not path.exists(parent_dir):
+            # if it does not exist, create it
+            mkdir(parent_dir)
+
+            # return false so that files are created
+            return False
+
+        # get the registry file path
+        registry_path = './lib/wordle_pickles/registry.pkl'
+    
+        # else check if the registry file exists
+        if path.exists(registry_path):
+
+            # load the registry list
+            registry = self._load_object('registry')
+
+            # loop through each item in the registry list
+            for file_prefix in registry:
+
+                # check if path exists to pkl file
+                if not path.exists(f'./lib/wordle_pickles/{file_prefix}.pkl'):
+
+                    # if a file does not exist, need to generate files
+                    return False
+            
+            # else return true as all files exist
+            return True
+        
+        # if directory exists but registry does not, return false and create files
+        else: 
+            return False
+            
+
+    def _get_word_banks(self) -> Tuple[list, list]:
+        # get the webpage source as text from the website
+        web_source_txt = get('https://www.nytimes.com/games/wordle/index.html').text
+
+        # extract the javascript file link from the page source
+        js_file_link = search('https://www.nytimes.com/games-assets/v2/wordle.[\w]{40}.js', web_source_txt).group()
+
+        # get the javascript file text from the link aquired in the previous step
+        js_file_txt = get(js_file_link).text
+
+        # get the banks from the javascript file
+        banks = findall('\[((["][a-z]{5}["][,]?){50,})\]', js_file_txt)
+
+        # remove the banks returned from tuples and fix formatting
+        word_order = banks[0][0].replace('"', '').split(',')
+        valid_words = banks[1][0].replace('"', '').split(',')
+
+        # return the two lists
+        return word_order, valid_words
+
+
+    def _get_wotd(self) -> str:
+        # create a wotd_scraper object. This will get the wotd on creation
+        scraper = WordleScraper()
+
+        # return the word of the day 
+        return scraper._wotd
+
+
+    def _get_todays_date(self) -> datetime:
+        # return todays date as a datetime object
+        return datetime.now().date()
+
+
+    def _map_date_to_word(self, word_order:list) -> dict[datetime, str]:
+        # get todays date as a datetime object
+        todays_date = self._get_todays_date()
+
+        # get todays word of the day
+        wotd = self._get_wotd()
+
+        # initialize timedelta to 1 day
+        delta = timedelta(days=1)
+
+        # get the index of todays word
+        wotd_index = word_order.index(wotd)
+
+        # find the date corresponding to the first 
+        starting_date = todays_date - (delta * (wotd_index))
+
+        # datetime object that will be used for mapping
+        map_date = starting_date
+
+        # initialize the dictionary to hold the mappings
+        d_to_w = dict()
+
+        # loop through each word in the word_order list
+        for word in word_order:
+
+            # map the word to its corresponding date
+            d_to_w[map_date] = word
+
+            # increment the date for the next word
+            map_date += delta
+
+        # return the mapped dictionary
+        return d_to_w
+
+
+    def _map_word_to_date(self, d_to_w:dict) -> dict[str, datetime]:
+        # initialize new dictionary
+        w_to_d = dict()
+
+        # loop through d_to_w dictionary and reverse mapping
+        for date, word in d_to_w.items():
+            # map word to date
+            w_to_d[word] = date
+
+        # return dictionary
+        return w_to_d
+
+
+    def _pkl_object(self, object:dict|list, file_prefix:str) -> None:
+        # path for the pickled
+        file_path = f'./lib/wordle_pickles/{file_prefix}.pkl'
+        
+        # open the file path to write the pickle data
+        with open(file_path, 'wb') as f:
+            
+            # write the pickled data to file_path
+            dump(object, f)
+
+
+    def _load_object(self, file_prefix:str) -> dict|list:
+        # get the file path for the pickled data
+        file_path = f'./lib/wordle_pickles/{file_prefix}.pkl'
+        
+        # open the pkl file
+        with open(file_path, 'rb') as f:
+            # load the pickled data
+            object = load(f)
+
+        # return the dictionary
+        return object  
+
+
+    def _store_d_to_w(self, d_to_w:dict[datetime, str]) -> list:
+        # for typing stuff
+        date:datetime
+
+        # registry list to store file prefixes
+        registry = list()
+
+        # initialize the dictionary
+        temp_dict = dict()
+
+        # get the starting current year. This is used check when we are done with the 
+        curr_year = list(d_to_w.keys())[0].year
+
+        # loop through each mapping in the d_to_w dictionary
+        for date, word in d_to_w.items():
+
+            # check if the year of the current date matches the date of the previous
+            if date.year == curr_year:
+                
+                # if in same year, add mapping to temp dictionary
+                temp_dict.update({date: word})
+
+            # else current date is in the next year, dump the current temp dictionary and start again with new year
+            else:
+                # pkl the temp dictionary
+                self._pkl_object(temp_dict, curr_year)
+
+                # add the file prefix to the registry list
+                registry.append(str(curr_year))
+
+                # clear the temp dictionary
+                temp_dict.clear()
+
+                # add the first entry for the new year
+                temp_dict.update({date: word})
+            
+            # set curr_year to the year date
+            curr_year = date.year
+
+        # pickle last temp_dict
+        self._pkl_object(temp_dict, curr_year)
+
+        # add the file prefix to the registry list
+        registry.append(str(curr_year))
+
+        # return the registry list
+        return registry
+
+
+    def _store_other(self, object:dict|list, prefix:str) -> str:
+        # pickle the object
+        self._pkl_object(object, prefix)
+
+        # return the prefix after the object as been successfully stored
+        return prefix
+
+
+    def _gen_files(self) -> None:
+        # get the word lists from the wordle website
+        word_order, valid_words = self._get_word_banks()
+
+        # add the words in word_order to valid words
+        # we found that the words in word_order are not contained in valid_words
+        valid_words.extend(word_order)
+
+        # sort list in alphabetical order
+        valid_words.sort()
+
+        # get the dictionary that maps dates to words
+        d_to_w = self._map_date_to_word(word_order)
+
+        # get the dictionary that maps words to dates
+        w_to_d = self._map_word_to_date(d_to_w)
+
+        # initialize registry list. This will store the prefixes of files so that
+        # class can determine if needed files exist or not
+        registry_list = list()
+
+        # store the word_order list to files and add file prefixes to registry list
+        registry_list.append(self._store_other(word_order, 'word_order'))
+
+        # store the d_to_w dictionary to files and add file prefixes to registry list
+        registry_list.extend(self._store_d_to_w(d_to_w))
+
+        # store w_to_d dictionary to file and add file prefix to the registry list
+        registry_list.append(self._store_other(w_to_d, 'reverse'))
+
+        # store the valid words to file and add the file prefix to the registry list
+        registry_list.append(self._store_other(valid_words, 'valid_words'))
+
+        # write registry list to pickle file for restart
+        self._pkl_object(registry_list, 'registry')
