49b46a619d23fd442151398942275e45c63dfa2b
m-schmutz <marschm131@gmail.com>
2022-09-20 20:29:12 -0700

    desktop progress on debug_logs

--- debug_logs.py
+++ debug_logs.py
@@ -8,9 +8,9 @@ LOG_RE = '\[[0-9\-: ]*\] Exception .*'
 TB_RE = '(\[[0-9\-: ]*\]\n([\S ]+\n)+)'
 TIME_RE = '[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}'
 
-class LogDebug:
+class LogReader:
     def __init__(self) -> None:
-        self.entries = self.match_entries()
+        self.entries = self.match_excs()
   
     @staticmethod
     def parse_bot_log() -> list[str]:
@@ -24,25 +24,25 @@ class LogDebug:
             tbs = findall(TB_RE, log.read())
         return tbs
 
-    def match_entries(self) -> dict[str, str]:
+    def match_excs(self) -> dict[str, str]:
         logs = self.parse_bot_log()
         tbs = self.parse_tb_log()
-
-        # print(len(logs))
-        print(tbs)
 
         entries = dict()
 
         for log, tb in zip(logs, tbs):
+
             log_time = search(TIME_RE, log).group()
             tbs_time = search(TIME_RE, tb[0]).group()
             assert(log_time == tbs_time)
 
-            entries[log_time] = (log[22:], tb[22:])
+            entries[log_time] = (log[22:], tb[0][22:])
+
+        return entries
 
 
-    def list_entries(self) -> None:
-        for time, logs in self.entries:
+    def list_exc_entries(self) -> None:
+        for time, logs in self.entries.items():
             print(f'{time}: {logs[0]}')
             print(logs[1])
 
