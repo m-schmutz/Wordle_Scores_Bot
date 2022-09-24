5d05130bfd6d4625dc4b7cb4d01aa73cd4be43e3
m-schmutz <marschm131@gmail.com>
2022-09-24 13:57:04 -0700

    creating _submit

--- run_bot.py
+++ run_bot.py
@@ -1,61 +1,67 @@
 #!./venv/bin/python3.10
 from lib import *
 
+async def _submit(bot:WordleBot, image:Attachment, interaction: Interaction) -> None:
+    
+    # Grab date of submission and try to score the game. If the game
+    # cannot be processed, reply with an error message and return.
+    date = interaction.created_at.astimezone().date()
+    game = bot.scoreGame(await image.read(), date)
+    
+
+    # Submit scores to database. If the user has already submit
+    # today, then reply with an error message and return.
+    baseStats, status = bot.db.submit_data(
+        username= str(interaction.user),
+        dtime= date,
+        win= game.won,
+        guesses= game.numGuesses,
+        greens= game.uniqueCorrect,
+        yellows= game.uniqueMisplaced,
+        uniques= game.uniqueAll)
+        
+    
+    # Reply to user's submission with stats.
+    await interaction.response.send_message(
+        embed= SubmissionEmbed(
+            username= interaction.user.name,
+            stats= baseStats),
+        file= await image.to_file(spoiler=True))
+
+    await interaction.followup.send(
+        content= bot.getResponse(
+            solved= game.won,
+            numGuesses= game.numGuesses),
+        ephemeral= True)
+
+    return
+
+
+
+
+
+
+
+
 def main() -> None:
     bot = WordleBot(server_id)
     slash_cmd = bot.tree.command
 
     @slash_cmd(description='Submit a screenshot of your Wordle game!', guild=bot.guild)
     async def submit(interaction: Interaction, image: Attachment) -> None:
-
-        # Grab date of submission and try to score the game. If the game
-        # cannot be processed, reply with an error message and return.
-        date = interaction.created_at.astimezone().date()
         try:
-            game = bot.scoreGame(await image.read(), date)
+            _submit(bot, image, interaction)
+
         except InvalidGame as e:
             # update log about invalid game
             return await interaction.response.send_message(content=e.message, ephemeral=True)
 
-        except:
-            exc_type, _, exc_traceback = exc_info()
-            return
-
-        # Submit scores to database. If the user has already submit
-        # today, then reply with an error message and return.
-        try:
-            baseStats, status = bot.db.submit_data(
-                username= str(interaction.user),
-                dtime= date,
-                win= game.won,
-                guesses= game.numGuesses,
-                greens= game.uniqueCorrect,
-                yellows= game.uniqueMisplaced,
-                uniques= game.uniqueAll)
-
-            # update log
-           
-            
         except DoubleSubmit as e:
             # update log about double submit
             return await interaction.response.send_message(content=e.message, ephemeral=True)
 
         except:
             exc_type, _, exc_traceback = exc_info()
-            return
-
-        # Reply to user's submission with stats.
-        await interaction.response.send_message(
-            embed= SubmissionEmbed(
-                username= interaction.user.name,
-                stats= baseStats),
-            file= await image.to_file(spoiler=True))
-
-        await interaction.followup.send(
-            content= bot.getResponse(
-                solved= game.won,
-                numGuesses= game.numGuesses),
-            ephemeral= True)
 
         
 
@@ -66,7 +72,7 @@ def main() -> None:
             await interaction.response.send_message(view= LinkView(), ephemeral= True)
 
         except:
-            exc_type, _, exc_traceback = exc_info()
+            
             return
 
     @slash_cmd(description='Roll an N-sided die!', guild=bot.guild)
