3d8c88d66ffa32e197be74e07b6ed371a01e83a8
m-schmutz <marschm131@gmail.com>
2022-09-24 19:27:22 -0700

    can get logs by user and event

--- lib/logdatabase.py
+++ lib/logdatabase.py
@@ -11,12 +11,20 @@ LOG_DB_PATH = './lib/logs/log.db'
 LOG_EVENTS = {1: 'submit', 2: 'new', 3: 'doublesub', 4: 'invalid', 5: 'rolldie', 6: 'link', 7: 'exception', 8: 'su/sd'}
 
 
+
+
 def dtime_to_dint(dtime:datetime) -> int:
     return int(dtime.strftime('%Y%m%d%H%M%S'))
 
 def dint_to_str(dint:int) -> str:
     dtime = datetime.strptime(str(dint), '%Y%m%d%H%M%S')
     return dtime.strftime('%m-%d-%Y %H:%M:%S')
+
+
+def format_entry(entry) -> str:
+    time, user, event, msg = entry
+    return f'[{dint_to_str(time)}] {user}, {event}: {msg}'
+
 
 
 class NoLogs(Exception):
@@ -92,29 +100,90 @@ class LogUpdate:
 
 
 
-
-
 class LogReader:
     def __init__(self) -> None:
-
+        # check that a log exists
         if not exists(LOG_DB_PATH):
             raise NoLogs
-
+        
+        # ensure that the database connection is closed on exit
         register(self.close_connection)
 
+        # connect to the log database
         self._log = connect(LOG_DB_PATH)
 
     def close_connection(self) -> None:
         # close connection to database
         self._log.close()
 
-    def logs_by_user(self, user:str) -> list:
-        pass
 
-    def logs_by_event(self) -> list:
-        print('Events: ')
+    def logs_by_user(self, file:str=None) -> list:
+        users = self._get_unique_users()
+        registry = dict()
+
+        print('Pick a user: ')
+
+        for i, user in users:
+            registry[i] = user
+            print(f'{i}: {user}')
+
+        try: 
+            user_ind = int(input('> '))
+            user = registry[user_ind]
+        except:
+            print('Invalid input')
+            return
+
+        entries = self._get_by_user(user)
+
+        print(entries)
+
+
+    def logs_by_event(self, file:str=None) -> list:
+        print('Pick an event to view: ')
         print('1: Game submissions\n2: New User added\n3: Double Submissions\n4: Invalid Games\n5: Die Rolls\n6: Link Requests\n7: Exceptions\n8: Startup/Shutdowns')
+        try:
+            event = int(input('> '))
+        except:
+            print('Invalid input')
+            return
 
+        entries = self._get_by_event(LOG_EVENTS[event])
+
+        print(entries)
+
+        
 
     def logs_by_timeframe(self) -> list:
         pass
+
+
+
+
+    def _get_by_event(self, event:str) -> list:
+
+        with self._log as _cur:
+            entries = _cur.execute(f"SELECT * FROM BotLog WHERE event = '{event}'").fetchall()
+        
+        formatted = list(map(format_entry, entries))
+
+        return formatted
+
+
+    def _get_by_user(self, user:str) -> list:
+
+        with self._log as _cur:
+            entries = _cur.execute(f"SELECT * FROM BotLog WHERE user = '{user}'").fetchall()
+        
+        formatted = list(map(format_entry, entries))
+
+        return formatted
+
+
+    def _get_unique_users(self) -> list:
+        with self._log as _cur:
+            users = _cur.execute('SELECT DISTINCT user from BotLog').fetchall()
+        
+        formatted = [(i, user[0]) for i, user in enumerate(users)]
+
+        return formatted
